6fab9e6e0881b20ee15e8b597b23a042f82f0cc4
mohammed benabdallah <simobend2005@gmail.com>
2025-11-03 00:31:09 +0100

    Correct variable declaration in calculateArea function
    Fix variable scope for length and width in calculateArea function.

--- calculate_Area.js
+++ calculate_Area.js
@@ -1,8 +1,8 @@
 let lenght;
 let width;
 function calculateArea (lenght, width){
-    lenght = parseFloat(document.getElementById('lenght').value);
-    width = parseFloat(document.getElementById('width').value);
+    let lenght = parseFloat(document.getElementById('lenght').value);
+    let width = parseFloat(document.getElementById('width').value);
 
     let area = length * width;
     document.getElementById('result').innerText =`The area of the rectangle is :${area}`;
